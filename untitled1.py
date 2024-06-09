63da5f0731b6e7531a3d8599fc4ec6dbb4275c35
paggy404 <paggy40408@gmail.com>
2024-06-10 01:16:53 +0800

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -13,6 +13,12 @@ from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage,TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
 
+import openai
+
+
+# 載入 LINE Message API 相關函式庫
+import json
+
 import os
 
 line_bot_api = LineBotApi(os.environ.get('Channel_Access_Token'))
@@ -26,6 +32,40 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    return 'OK'
+
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
+    try:
+        line_bot_api = LineBotApi(os.environ.get('Channel_Access_Token'))
+        handler = WebhookHandler('fb63a6a67e93c96b850177b269a0369b')
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        tk = json_data['events'][0]['replyToken']
+        msg = json_data['events'][0]['message']['text']
+        # 取出文字的前五個字元，轉換成小寫
+        ai_msg = msg[:6].lower()
+        reply_msg = ''
+        # 取出文字的前五個字元是 hi ai:
+        if ai_msg == '我不舒服':
+            openai.api_key = 'sk-proj-0AgzlBVg2DDfSBLWkECkT3BlbkFJ29hyh38Vwbv89RujdEcV'
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                model='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = msg
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(tk,text_message)
+    except:
+        print('error')
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
